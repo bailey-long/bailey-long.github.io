ef9b2f142710ae81d550d9acb4a55ce3661af17b
Bailey Long <bailey-long@live.nmit.ac.nz>
2022-10-13 12:45:40 +1300

    updated mobile navbar

--- src/nav.js
+++ src/nav.js
@@ -1,24 +1,3 @@
-$(document).ready(function(){
-    //ANCHOR Slideup navbar on mobile by default and when new page is loaded in
-    if (window.innerWidth < 830){
-        $(".navBar").slideUp();
-    }
-    //ANCHOR Functionality for clicking nav button
-    $(".navDropdown").click(function(){
-        if($(".navBar").first().is(":hidden")){
-            $(".navBar").slideDown("slow");
-        } else {
-            $(".navBar").slideUp("slow");
-        }    
-    });
-    //ANCHOR hide nav when page size changes (ie rotate mobile device, move into developer tools)
-    $(window).resize(function() {
-        if (window.innerWidth < 830){
-            $(".navBar").slideUp();
-        } else {
-            if($(".navBar").first().is(":hidden")){
-                $(".navBar").slideDown();
-            }
-        }
-    });
-});
+document.getElementsByClassName('navDropdown')[0].onclick = function() {
+    document.getElementsByClassName('navBar')[0].classList.toggle('hide');
+};
