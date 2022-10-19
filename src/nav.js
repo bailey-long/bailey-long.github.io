defb425362552ca6477c302320165d57eefa6c23
Bailey Long <bailey-long@live.nmit.ac.nz>
2022-10-19 23:44:37 +1300

    Updated page content and styling

--- src/nav.js
+++ src/nav.js
@@ -1,3 +1,22 @@
+//Mobile navbar dropdown
 document.getElementsByClassName('navDropdown')[0].onclick = function() {
     document.getElementsByClassName('navBar')[0].classList.toggle('hide');
 };
+//Bring mobile navbar up on page load
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementsByClassName('navBar')[0].classList.toggle('hide');
+});
+//Insert footer content into pages
+document.getElementsByClassName("footerContainer")[0].innerHTML = `
+<footer>
+<div class="links">
+<h3> Site Map </h3>
+    <a href="/src/index/index.html"><p>Home</p></a>
+    <a href="/src/about/about.html"><p>About</p></a>
+    <a href="/src/contact/contact.html"><p>Contact</p></a>
+    <a href="/src/hobbies/hobbies.html"><p>Hobbies</p></a>
+    <a href="/src/game/game.html"><p>Top Secret</p></a>
+</div>
+<div class="copyright"><p>© 2022 Bailey Long</p></div>
+</footer>
+`
